133211ff1694658c63e5051cf961a91c39467425
Coder2195 <coder2195mail@gmail.com>
2025-05-17 13:05:47 -0400

    arch failed :(

--- .config/awesome/autostart.sh
+++ .config/awesome/autostart.sh
@@ -6,9 +6,11 @@ function run {
   fi
 }
 
-run picom
+killall picom;
+picom -b;
 run flameshot
 run copyq --start-server
 run discord
 run gnome-keyring-daemon
 run fcitx5
+run slimbookbattery --minimize
